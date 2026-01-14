47c117aac170657edb2fd36c0d9c2edca10fd3d7
sYanXO <sreayan69@gmail.com>
2026-01-14 20:23:25 +0530

    added worker pool with channel and goroutine, missing persistence maybe json or repl style cli idk

--- main.go
+++ main.go
@@ -2,23 +2,40 @@ package main
 import(
 	"fmt"
 	"os"
+	"time"
 )
 type Task struct{
 	ID int
 	Name string
+	Status string
 }
 
 var tasks []Task
+
+var TaskQueue = make(chan *Task) // create a channel named TaskQueue that can only parse and store Task type data 
 
 func addTask(name string) {
 		task:= Task{
 				ID: len(tasks)+1,
 				Name : name,
+				Status : "Pending",
 		}	
 		tasks = append(tasks,task)
 }	
 
+func worker(id int, tasks <-chan *Task){
+	for task := range tasks {
+		fmt.Printf("Worker %d started task %d\n", id, task.ID)
+		task.Status = "Running"
+		
 
+	time.Sleep(2*time.Second)
+
+	task.Status = "Completed"
+	fmt.Printf("Worker %d finished task %d\n", id, task.ID)
+}
+
+}
 
 func main(){
 	fmt.Println("This is a task manager . Welcome v0")
@@ -33,6 +50,10 @@ func main(){
 
 	command := args[1]
 	fmt.Println("command :",command)
+
+	for i:=1; i<=3; i++ {
+		go worker(i,TaskQueue)
+	}
 
 	switch command {
 		case "add" :
@@ -58,10 +79,17 @@ func main(){
 /////////////////////////////////////////////////////////////////////
 		
 	case "run" :
-		fmt.Println("Running tasks (sequentially)")
-		for _, task := range tasks {
-			fmt.Println("Executing:", task.Name)
+		if len(tasks) == 0{
+			fmt.Println("There are no tasks to run")
+			return
 		}
+		for i := range tasks{
+				TaskQueue <- &tasks[i]
+			}
+
+			fmt.Println("task dispatched to workers")
+
+			time.Sleep(5*time.Second)
 ////////////////////////////////////////////////////////////////////
 	default:
 		fmt.Println("Unrealised command : ",command)
